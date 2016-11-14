b8be5c5493e897871262b1a040a8f21b37198168
Aaron Chang <amc120@duke.edu>
2016-11-14 00:41:02 -0500

    implemented moveSingleEnemy

--- src/gameplayer/model/GamePlayModel.java
+++ src/gameplayer/model/GamePlayModel.java
@@ -108,13 +108,31 @@ public class GamePlayModel {
 				}
 			}
 		}
-		
 	}
 	
 	//get direction
 	
 	
 	private void moveSingleEnemy(Enemy e){
+		//to make it easier, only updating enemy's current cell once it reaches the center point of the next cell
+		double moveDist = e.getMovingSpeed();
+		double distToMove = (Math.abs(cellToCoordinate(e.getCurrentCell().getNext().getX()) - e.getX()) + 
+				Math.abs(cellToCoordinate(e.getCurrentCell().getNext().getY()) - e.getY()));
+		while (moveDist > 0) {
+			if (moveDist >= distToMove) { //can move to center of next cell
+				e.setX(e.getX() + e.getxDirection() * distToMove);
+				e.setY(e.getY() + e.getyDirection() * distToMove);
+				e.setCurrentCell(e.getCurrentCell().getNext());
+				e.setxDirection(e.getCurrentCell().getNext().getX() - e.getCurrentCell().getX()); //-1, 0, or 1
+				e.setyDirection(e.getCurrentCell().getNext().getY() - e.getCurrentCell().getY());
+				moveDist -= distToMove;
+			}
+			else {
+				e.setX(e.getX() + e.getxDirection() * moveDist);
+				e.setY(e.getY() + e.getyDirection() * moveDist);
+			}
+			
+		}
 		
 		
 		// sub life when enter the end
@@ -128,7 +146,7 @@ public class GamePlayModel {
 		
 		
 		//enter new enemy
-		if(this.nextEnteringEnemy != null){
+		if(this.nextEnteringEnemy != null) {
 			enemyOnGrid.add(this.nextEnteringEnemy);
 			this.nextEnteringEnemy.setCurrentCell(this.startPoint);
 		}
