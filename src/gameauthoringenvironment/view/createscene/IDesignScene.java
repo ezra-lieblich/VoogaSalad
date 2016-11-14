9fb6d4257ba1c6f065d1b3fa023e59a539e12e90
Kayla Schulz <kms114@duke.edu>
2016-11-14 08:02:26 -0500

    updates to interfaces

--- src/gameauthoringenvironment/view/createscene/IDesignScene.java
+++ src/gameauthoringenvironment/view/createscene/IDesignScene.java
@@ -1,5 +1,9 @@
 package gameauthoringenvironment.view.createscene;
 
+import javafx.scene.Group;
+import javafx.scene.Scene;
+
+
 /**
  * An interface to define the external API in regards to the creation of
  * the authoring screen.
@@ -7,6 +11,8 @@ package gameauthoringenvironment.view.createscene;
  */
 public interface IDesignScene {
 
-    
-    
+    public Scene getScene ();
+
+    public Group getMyRoot ();
+
 }
---
+++ src/gameauthoringenvironment/view/path/IPathView.java
@@ -0,0 +1,11 @@
+package gameauthoringenvironment.view.path;
+
+/**
+ * 
+ * This interface details the components of the path tab. The interface defines our internal API
+ * and the public methods defining what components the path view should contain.
+ *
+ */
+public interface IPathView {
+
+}
