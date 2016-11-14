e283b59089b2189a6d63c66e7babd1275ab7846d
Kayla Schulz <kms114@duke.edu>
2016-11-14 02:09:33 -0500

    Beginning code interfaces

--- src/gameauthoringenvironment/view/createscene/IDesignScene.java
+++ src/gameauthoringenvironment/view/createscene/IDesignScene.java
@@ -1,5 +1,12 @@
 package gameauthoringenvironment.view.createscene;
 
+/**
+ * An interface to define the external API in regards to the creation of
+ * the authoring screen.
+ *
+ */
 public interface IDesignScene {
 
+    
+    
 }
