4a775b9b1f6d1ff34f42c08b45a2b63a9450dcd1
Sean Hudson <srh50@duke.edu>
2016-11-13 23:51:49 -0500

    added usecases package

---
+++ src/usecases/gameEngine.java
@@ -0,0 +1,19 @@
+package usecases;
+
+public class gameEngine {
+    ILevelUpdater levelUpdater;
+    
+    
+    /**
+     * This method will take in input from the game authoring environment and will call a method in the ILevelUpdater
+     * interface to add a new enemy amount.
+     * 
+     * @param enemyType
+     * @param amount
+     */
+    public void setEnemiesPerLevel(String enemyType, int amount) {
+        levelUpdater.setEnemiesPerLevel(enemyType, amount);
+    }
+    
+    
+}
