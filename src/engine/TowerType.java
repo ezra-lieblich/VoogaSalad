4a2250a164b02988553d7efc088c16e7dd8f1236
andrewbihl <andrewbihlva@gmail.com>
2016-11-16 17:18:03 -0500

    add some values to tower

--- src/engine/TowerType.java
+++ src/engine/TowerType.java
@@ -1,7 +1,13 @@
 package engine;
 
 public class TowerType extends Entity {
-
+	
+	private String name;
+	private WeaponType weapon;
+	private String imagePath;
+	private int cost;
+	
+	
     TowerType () {
         super();
         // TODO Auto-generated constructor stub
