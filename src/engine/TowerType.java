4971ecba1aaa1094fb5cac4cfcf546968b93769b
andrewbihl <andrewbihlva@gmail.com>
2016-11-16 17:22:52 -0500

    Add more properties to tower

--- src/engine/TowerType.java
+++ src/engine/TowerType.java
@@ -5,8 +5,9 @@ public class TowerType extends Entity {
 	private String name;
 	private WeaponType weapon;
 	private String imagePath;
-	private int cost;
-	
+	private int price;
+	private int sellPrice;
+	private int unlockLevel;
 	
     TowerType () {
         super();
