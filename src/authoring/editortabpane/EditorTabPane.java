0d92f99579cb46ac51f4af5433cb180f0aab3704
Kayla Schulz <kms114@duke.edu>
2016-11-18 04:56:25 -0500

    Continued work on tab sidebar. Event handlers and toggle settings

--- src/authoring/editortabpane/EditorTabPane.java
+++ src/authoring/editortabpane/EditorTabPane.java
@@ -1,6 +1,7 @@
 package authoring.editortabpane;
 
 import java.util.ResourceBundle;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Side;
@@ -8,9 +9,11 @@ import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 
 
 /**
@@ -44,16 +47,45 @@ public class EditorTabPane implements IEditorTabPane {
         buildRibbonMenu();
     }
 
-    private Button enemyButton;
     private void buildRibbonMenu () {
 
-        enemyButton = buildButton(GUIResources.getString("Enemy"), "enemy.png");
-        Button towerButton = buildButton(GUIResources.getString("Tower"), "tower.png");
-        Button pathButton = buildButton(GUIResources.getString("Path"), "path.png");
-        Button weaponsButton = buildButton(GUIResources.getString("Weapons"), "weapons.png");
-        Button settingsButton =
-                buildButton(GUIResources.getString("GameSettings"), "gamesettings.png");
-        Button levelButton = buildButton(GUIResources.getString("Level"), "level.png");
+        ToggleButton enemyButton =
+                buildButton(GUIResources.getString("Enemy"), "enemy.png",
+                            event -> openView(GUIResources.getString("Enemy")));
+        ToggleButton towerButton =
+                buildButton(GUIResources.getString("Tower"), "tower.png",
+                            event -> openView(GUIResources.getString("Tower")));
+        ToggleButton pathButton =
+                buildButton(GUIResources.getString("Path"), "path.png",
+                            event -> openView(GUIResources.getString("Path")));
+        ToggleButton weaponsButton =
+                buildButton(GUIResources.getString("Weapons"), "weapons.png",
+                            event -> openView(GUIResources.getString("Weapons")));
+        ToggleButton settingsButton =
+                buildButton(GUIResources.getString("GameSettings"), "gamesettings.png",
+                            event -> openView(GUIResources.getString("GameSettings")));
+        ToggleButton levelButton =
+                buildButton(GUIResources.getString("Level"), "level.png",
+                            event -> openView(GUIResources.getString("Level")));
+        
+        ToggleGroup group1 = new ToggleGroup();
+        group1.selectedToggleProperty()
+                // Set Change Text if toggled
+                .addListener( (ObservableValue<? extends Toggle> ov,
+                               Toggle old_toggle,
+                               Toggle new_toggle) -> {
+                    if (new_toggle == null)
+                        return;
+                    if (new_toggle.isSelected()) {
+                    }
+                });
+        enemyButton.setToggleGroup(group1);
+        towerButton.setToggleGroup(group1);
+        pathButton.setToggleGroup(group1);
+        weaponsButton.setToggleGroup(group1);
+        settingsButton.setToggleGroup(group1);
+        levelButton.setToggleGroup(group1);
+        
 
         buildTabs(enemyButton);
         buildTabs(towerButton);
@@ -64,15 +96,19 @@ public class EditorTabPane implements IEditorTabPane {
 
     }
 
-    private void buildTabs (Button button) {
+    private void openView (String text) {
+        System.out.println(text);
+    }
+
+    private void buildTabs (ToggleButton button) {
         Tab tab = new Tab();
         tab.setGraphic(button);
         tab.setClosable(false);
         tabPane.getTabs().add(tab);
     }
 
-    private Button buildButton (String text, String fileName) {
-        Button button = new Button();
+    private ToggleButton buildButton (String text, String fileName, EventHandler<ActionEvent> handler) {
+        ToggleButton button = new ToggleButton();
         button.setText(text);
         Image image = new Image(getClass().getClassLoader().getResourceAsStream(fileName));
         ImageView curImage = new ImageView(image);
@@ -81,13 +117,7 @@ public class EditorTabPane implements IEditorTabPane {
         button.setGraphic(curImage);
         button.setGraphicTextGap(5);
         button.setMinSize(120, 30);
-        button.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-                //send to applicationController with String text to determine which scene to open
-            }
-        });
+        button.setOnAction(handler);
         return button;
     }
 
