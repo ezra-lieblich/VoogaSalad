c6c304753469af105cc5af991846e7c7f65fe92e
Kayla Schulz <kms114@duke.edu>
2016-11-21 02:16:03 -0500

    deleted orig files

--- src/authoring/editorview/weapon/WeaponDataSource.java
+++ src/authoring/editorview/weapon/WeaponDataSource.java
@@ -7,12 +7,17 @@ package authoring.editorview.weapon;
 public interface WeaponDataSource {
 
     public void setWeaponImage (int weaponID, int weaponImageID);
-    public void setWeaponDamage(int weaponID, int damage);
-    public void setWeaponRange(int weaponID, int range);
-    
-    public int createWeapon();
-    public int getWeaponImage(int weaponID);
-    public int getWeaponDamage(int weaponID);
-    public int getWeaponRange(int weaponID);
-    
+
+    public void setWeaponDamage (int weaponID, int damage);
+
+    public void setWeaponRange (int weaponID, int range);
+
+    public int createWeapon ();
+
+    public int getWeaponImage (int weaponID);
+
+    public int getWeaponDamage (int weaponID);
+
+    public int getWeaponRange (int weaponID);
+
 }
