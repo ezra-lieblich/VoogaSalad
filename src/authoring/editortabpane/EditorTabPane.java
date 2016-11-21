76866ee9fc688da13f16cbab94b0725ce7698f88
andrewbihl <andrewbihlva@gmail.com>
2016-11-21 01:46:24 -0500

    Change process of creation of tab pane

--- src/authoring/editortabpane/EditorTabPane.java
+++ src/authoring/editortabpane/EditorTabPane.java
@@ -1,7 +1,7 @@
 package authoring.editortabpane;
 
 import java.util.Enumeration;
-import java.util.ResourceBundle;
+import java.util.List;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -26,17 +26,14 @@ import javafx.scene.image.ImageView;
 public class EditorTabPane implements IEditorTabPane {
 
     private TabPane tabPane;
-    private ResourceBundle GUIResources;
     private EditorTabPaneDelegate delegate;
     private String viewToOpen;
+    private List<String> tabNames;
 
-    public EditorTabPane (int width, int height) {
+    public EditorTabPane (int width, int height, List<String> tabs) {
         viewToOpen = "Enemy"; // default, change this later
-        String initFile = "resources";
-        String fileName = "/GameAuthoringToolbar";
-        GUIResources = ResourceBundle.getBundle(initFile + fileName);
         tabPane = new TabPane();
-
+        tabNames = tabs;
         tabPane.setSide(Side.RIGHT);
         tabPane.setTabMaxHeight(120);
         tabPane.setTabMaxWidth(30);
@@ -60,11 +57,9 @@ public class EditorTabPane implements IEditorTabPane {
                     }
                 });
 
-        Enumeration<String> nextVal = GUIResources.getKeys();
-        while (nextVal.hasMoreElements()) {
-            String editorName = nextVal.nextElement();
-            ToggleButton button = buildButton(editorName, editorName.toLowerCase() + ".png",
-                                              event -> delegate.userSelectedTab(editorName));
+        for(String name: tabNames) {
+            ToggleButton button = buildButton(name, name.toLowerCase() + ".png",
+                                              event -> delegate.userSelectedTab(name));
             button.setToggleGroup(group1);
             buildTabs(button);
         }
