a24ffae6b07124016d39f6f38443111270e26af4
Diane Hadley <dfh12@duke.edu>
2016-11-28 17:46:03 -0500

    Add methods to be implemented by backend for Level settings

--- src/authoring/editorview/level/LevelDataSource.java
+++ src/authoring/editorview/level/LevelDataSource.java
@@ -1,5 +1,6 @@
 package authoring.editorview.level;
 
+import java.util.List;
 import java.util.Map;
 
 
@@ -7,7 +8,7 @@ import java.util.Map;
  * 
  * Interface to communicate with the game engine about changes in the level settings
  * 
- * @author Andrew Bihl
+ * @author Andrew Bihl, Diane Hadley
  */
 public interface LevelDataSource {
 
@@ -15,15 +16,32 @@ public interface LevelDataSource {
      * Sets creeps in the game engine
      * 
      * @param levelIndex
-     * @param map of creeps
+     * @param map of creeps, key = enemyID, value = number of that enemy
      */
-    // TODO: Create Creeps
-    // public void setCreeps (int levelIndex, Map<Creep, Integer> creeps);
+    public void setEnemies (int levelIndex, Map<Integer, Integer> enemyMap);
 
     public void setRewardPoints (int levelIndex, int winPoints);
 
     public void setRewardMoney (int levelIndex, int winMoney);
 
     public void setRewardHealth (int levelIndex, int winHealth);
+    
+    public void setPath (int levelIndex, int pathID);
+    
+    public void setTowers (int levelIndex, List<Integer> towerIDList);
+    
+    public void getEnemies (int levelIndex);
+    
+    public void getRewardPoints (int levelIndex);
+    
+    public void getRewardMoney (int levelIndex);
+    
+    public void getRewardHealth (int levelIndex);
+    
+    public void getPath (int levelIndex);
+    
+    public void getTowers (int levelIndex);
+    
+    
 
 }
