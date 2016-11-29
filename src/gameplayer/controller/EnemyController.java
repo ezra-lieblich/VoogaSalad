f99a124edfd74a3e6518d2f4763dd1cfe524ebf4
Lucy Zhang <lz107@duke.edu>
2016-11-28 19:06:57 -0500

    Updated enemycontroller

--- src/gameplayer/controller/EnemyController.java
+++ src/gameplayer/controller/EnemyController.java
@@ -11,12 +11,15 @@ import gameplayer.model.EnemyModel;
 import gameplayer.model.GamePlayModel;
 import gameplayer.view.GridGUI;
 import gameplayer.view.entity.EnemyView;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 
 public class EnemyController implements Observer{
 	private EnemyModel model;
 	//private List<EnemyView> enemyViews;
 	private HashMap<Enemy, EnemyView> enemyToView;
 	private GridGUI grid;
+	
 	
 	//TODO: enemy model instead of GamePlayModel
 	public EnemyController(EnemyModel model, GridGUI grid){
@@ -31,6 +34,7 @@ public class EnemyController implements Observer{
 		return this.model;
 	}
 	
+	/*
 	private void createEnemyViews(){
 		ArrayList<Enemy> enemies = (ArrayList<Enemy>) this.model.getEnemyList();
 		for (int i=0; i<enemies.size(); i++){
@@ -40,14 +44,22 @@ public class EnemyController implements Observer{
 			this.enemyToView.put(enemy, enemyView);
 		}
 	}
+	*/
+	
 	
 	private void updateEnemyViews(){
 		ArrayList<Enemy> enemies = (ArrayList<Enemy>) this.model.getEnemyList();
 		//if (enemies.size()<this.enemyToView.size()){
-		this.enemyToView.keySet().retainAll(enemies); //delete anything not used
+		//this.enemyToView.keySet().retainAll(enemies); //delete anything not used
 		//}
+		((Pane) this.grid.getGrid()).getChildren().clear();
 		for (int i=0; i<enemies.size(); i++){ //ALERT TODO: I don't think any keys will be the same bc as soon as the enemy object is 
 			//modified, it will no longer be the same key and no imageview will correspond to it, may need a unique id
+			Enemy enemy = enemies.get(i);
+			EnemyView enemyView = new EnemyView(enemy.getImage(), enemy.getWidth(), enemy.getHeight(), enemy.getHealth());
+		
+			((Pane) this.grid.getGrid()).getChildren().add(enemyView.getEnemyView());
+			/*
 			Enemy enemy = enemies.get(i);
 			if (this.enemyToView.containsKey(enemies.get(i))){ //update the coordinates
 				this.enemyToView.get(enemy).getEnemyView().setX(enemy.getxDirection());
@@ -58,7 +70,9 @@ public class EnemyController implements Observer{
 				enemyView.getEnemyView().setX(enemy.getxDirection());
 				enemyView.getEnemyView().setY(enemy.getyDirection());
 			}
+			*/
 		}
+		
 		/*
 		ArrayList<Enemy> enemies = (ArrayList<Enemy>) this.model.getEnemyList();
 		for (int i=0; i<enemies.size(); i++){
