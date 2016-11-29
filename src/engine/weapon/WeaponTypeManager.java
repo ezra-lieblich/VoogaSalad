c4a901206c92ac38fc85359b7466d8fb1eb6e936
Sean Hudson <srh50@duke.edu>
2016-11-29 01:07:41 -0500

    Added removeEnemy method to EnemyManager

--- src/engine/weapon/WeaponTypeManager.java
+++ src/engine/weapon/WeaponTypeManager.java
@@ -9,6 +9,7 @@ import engine.enemy.EnemyManager;
 
 public class WeaponTypeManager extends AbstractTypeManager<Weapon> implements WeaponManager {
     
+    @Override
     public void visitRemoveEntry(EnemyManager manager, Integer index) {
         applyToAllEntities(a -> a.removeTarget(index));
     }
