a611f73f9b02c48e7f75f9fc8747434646475981
Ezra <eml36@duke.edu>
2016-11-29 17:12:17 -0500

    creatted EnemyManagerController

--- src/engine/enemy/EnemyTypeManagerController.java
+++ src/engine/enemy/EnemyTypeManagerController.java
@@ -2,120 +2,107 @@ package engine.enemy;
 
 import authoring.editorview.enemy.EnemyDataSource;
 import authoring.editorview.enemy.IEnemyUpdateView;
+import authoring.editorview.path.IPathUpdateView;
+import engine.AbstractTypeManagerController;
+import engine.path.Path;
+import engine.path.PathBuilder;
+import engine.path.PathManager;
+import engine.path.PathManagerController;
 
 import java.util.List;
 
 /**
  * Created by ezra on 11/29/16.
  */
-public class EnemyTypeManagerController implements EnemyDataSource{
-    private EnemyManager enemyManager;
+public class EnemyTypeManagerController  
+	extends AbstractTypeManagerController<EnemyManager, EnemyBuilder, Enemy, IEnemyUpdateView> implements EnemyManagerController {
 
-    public EnemyTypeManagerController(EnemyManager enemyManager) {
-        this.enemyManager = enemyManager;
-    }
-    @Override
-    public int createEnemy(IEnemyUpdateView updateEnemyInterface) {
-        return 0;
-    }
+	EnemyTypeManagerController(EnemyManager enemyManager) {
+		super(enemyManager, new EnemyTypeBuilder());
+	}
 
-    @Override
-    public void deleteEnemy(int enemyID) {
+	@Override
+	public void setEnemySpeed(int enemyID, double enemySpeed) {
+		// TODO Auto-generated method stub
+		
+	}
 
-    }
+	@Override
+	public void setEnemyHealth(int enemyID, double enemyHealth) {
+		// TODO Auto-generated method stub
+		
+	}
 
-    @Override
-    public void setEnemyName(int enemyID, String enemyName) {
+	@Override
+	public void setEnemyDamage(int enemyID, double enemyDamage) {
+		// TODO Auto-generated method stub
+		
+	}
 
-    }
+	@Override
+	public void setEnemyRewardMoney(int enemyID, double enemyRewardMoney) {
+		// TODO Auto-generated method stub
+		
+	}
 
-    @Override
-    public void setEnemySize(int enemyID, double enemyScaleSize) {
+	@Override
+	public void setEnemyRewardPoints(int enemyID, double enemyRewardPoints) {
+		// TODO Auto-generated method stub
+		
+	}
 
-    }
+	@Override
+	public void setEnemyCollisionEffect(int enemyID, String enemyCollisionEffect) {
+		// TODO Auto-generated method stub
+		
+	}
 
-    @Override
-    public void setEnemyImage(int enemyID, String enemyImagePath) {
+	@Override
+	public double getEnemySpeed(int enemyID) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
-    }
+	@Override
+	public double getEnemyHealth(int enemyID) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
-    @Override
-    public void setEnemySpeed(int enemyID, double enemySpeed) {
+	@Override
+	public double getEnemyDamage(int enemyID) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
-    }
+	@Override
+	public double getEnemyRewardMoney(int enemyID) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
-    @Override
-    public void setEnemyHealth(int enemyID, double enemyHealth) {
+	@Override
+	public double getEnemyRewardPoints(int enemyID) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
-    }
+	@Override
+	public String getEnemyCollisionEffect(int enemyID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    @Override
-    public void setEnemyDamage(int enemyID, double enemyDamage) {
+	@Override
+	public List<Integer> getCreatedEnemyIDs() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    }
-
-    @Override
-    public void setEnemyRewardMoney(int enemyID, double enemyRewardMoney) {
-
-    }
-
-    @Override
-    public void setEnemyRewardPoints(int enemyID, double enemyRewardPoints) {
-
-    }
-
-    @Override
-    public void setEnemyCollisionEffect(int enemyID, String enemyCollisionEffect) {
-
-    }
-
-    @Override
-    public String getEnemyImage(int enemyID) {
-        return null;
-    }
-
-    @Override
-    public String getEnemyName(int enemyID) {
-        return null;
-    }
-
-    @Override
-    public double getEnemySpeed(int enemyID) {
-        return 0;
-    }
-
-    @Override
-    public double getEnemySize(int enemyID) {
-        return 0;
-    }
-
-    @Override
-    public double getEnemyHealth(int enemyID) {
-        return 0;
-    }
-
-    @Override
-    public double getEnemyDamage(int enemyID) {
-        return 0;
-    }
-
-    @Override
-    public double getEnemyRewardMoney(int enemyID) {
-        return 0;
-    }
-
-    @Override
-    public double getEnemyRewardPoints(int enemyID) {
-        return 0;
-    }
-
-    @Override
-    public String getEnemyCollisionEffect(int enemyID) {
-        return null;
-    }
-
-    @Override
-    public List<Integer> getCreatedEnemyIDs() {
-        return null;
-    }
+	@Override
+	protected EnemyBuilder constructTypeProperties(IEnemyUpdateView updateView, EnemyBuilder typeBuilder) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+   
 }
