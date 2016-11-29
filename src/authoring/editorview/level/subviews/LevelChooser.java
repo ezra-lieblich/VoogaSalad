6736319c900b50cae5e2d717a4c54f5354689da0
Diane Hadley <dfh12@duke.edu>
2016-11-28 20:40:48 -0500

    Start work on subviews for Level

---
+++ src/authoring/editorview/level/subviews/LevelChooser.java
@@ -0,0 +1,14 @@
+package authoring.editorview.level.subviews;
+
+public class LevelChooser {
+	
+	
+	private void buildLevelComboBox(){
+		
+	}
+	
+	private void displayLevelToUpdate(){
+		
+	}
+
+}
