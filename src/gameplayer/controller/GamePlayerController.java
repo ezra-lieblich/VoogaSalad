9d260eacbdd68828207294b0d279bd263073fabc
Lucy Zhang <lz107@duke.edu>
2016-11-29 17:33:01 -0500

    Added UI to trigger new level

--- src/gameplayer/controller/GamePlayerController.java
+++ src/gameplayer/controller/GamePlayerController.java
@@ -11,6 +11,7 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
@@ -42,6 +43,7 @@ public class GamePlayerController implements Observer {
 	private EnemyController enemyController;
 	private EnemyModel enemyModel;
 	private DragDropController dropController;
+	private double oldLevel;
 
 	private Queue<Enemy> currentWave;
 
@@ -53,6 +55,7 @@ public class GamePlayerController implements Observer {
 		this.model = new GamePlayModel(this.loader);
 		this.enemyModel = new EnemyModel(this.model);
 		this.model.addObserver(this);
+		this.oldLevel = 0;
 	}
 
 	/**
@@ -72,6 +75,8 @@ public class GamePlayerController implements Observer {
 		System.out.println("Settings: " + settings);
 		initGUI();
 		this.enemyController = new EnemyController(this.enemyModel, this.view.getGrid());
+		
+		
 	}
 
 	// For testing only
@@ -129,10 +134,20 @@ public class GamePlayerController implements Observer {
 	@Override
 	public void update(Observable o, Object arg) {
 		if (o instanceof GamePlayModel) {
+			double newLevel = ((GamePlayModel) o).getCurrentLevel();
 			// update level in display
 			this.view.updateStatsDisplay(((GamePlayModel) o).getGold(), ((GamePlayModel) o).getLife(),
 					((GamePlayModel) o).getCurrentLevel());
 			this.view.updateCurrentLevelStats(((GamePlayModel) o).getCurrentLevel());
+			if (this.oldLevel != newLevel){
+				//test level
+				this.oldLevel = newLevel;
+				this.view.newLevelPopUp(e->{
+					System.out.println("New level");
+					this.view.getGrid().getGrid().getChildren().clear();
+					//do something to trigger new level here!
+				});
+			}
 		}
 		/*
 		 * GamePlayModel model = (GamePlayModel) o; if (model.getLife() == 0) {
