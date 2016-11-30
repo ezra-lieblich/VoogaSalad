871951d1e6fd8f9d4c7a3604225b95fd1e8f53fb
Ezra <eml36@duke.edu>
2016-11-29 20:26:41 -0500

    added controller for level. Still need to implement frequency and path for level. Also see ILevelUpdateView to change parameters

--- src/engine/level/LevelTypeManagerController.java
+++ src/engine/level/LevelTypeManagerController.java
@@ -25,80 +25,76 @@ public class LevelTypeManagerController
 
 	@Override
 	public void setRewardMoney(int levelID, double winMoney) {
-		// TODO Auto-generated method stub
-		
+		getTypeManager().getEntity(levelID).setRewardMoney(winMoney);
 	}
 
 	@Override
 	public void setRewardHealth(int levelID, double winHealth) {
-		// TODO Auto-generated method stub
-		
+		getTypeManager().getEntity(levelID).setRewardHealth(winHealth);
 	}
 
 	@Override
 	public void setPath(int levelID, int pathID) {
-		// TODO Auto-generated method stub
+		// TODO Implement Path
 		
 	}
 
 	@Override
 	public void setTransitionTime(int levelID, double time) {
-		// TODO Auto-generated method stub
-		
+		getTypeManager().getEntity(levelID).setDurationInSeconds(time);
 	}
 
 	@Override
 	public Map<Integer, Integer> getEnemies(int levelID) {
-		// TODO Auto-generated method stub
-		return null;
+		return getTypeManager().getEntity(levelID).getEnemyCounts();
 	}
 
 	@Override
 	public double getRewardScore(int levelID) {
-		// TODO Auto-generated method stub
-		return 0;
+		return getTypeManager().getEntity(levelID).getRewardScore();
 	}
 
 	@Override
 	public double getRewardMoney(int levelID) {
-		// TODO Auto-generated method stub
-		return 0;
+		return getTypeManager().getEntity(levelID).getRewardMoney();
 	}
 
 	@Override
 	public double getRewardHealth(int levelID) {
-		// TODO Auto-generated method stub
-		return 0;
+		return getTypeManager().getEntity(levelID).getRewardHealth();
 	}
 
 	@Override
 	public int getPath(int levelID) {
-		// TODO Auto-generated method stub
+		// TODO Implement Path
 		return 0;
 	}
 
 	@Override
 	public double getTransitionTime(int levelID) {
-		// TODO Auto-generated method stub
-		return 0;
+		return getTypeManager().getEntity(levelID).getDurationInSeconds();
 	}
 
 	@Override
 	public void setEnemyFrequency(int levelID, int enemyID, double enemyFrequency) {
-		// TODO Auto-generated method stub
+		// TODO Need to implement 
 		
 	}
 
 	@Override
 	public double getEnemyFrequency(int levelID, int enemyID) {
-		// TODO Auto-generated method stub
+		// TODO Need to implement
 		return 0;
 	}
 
 	@Override
 	protected LevelBuilder constructTypeProperties(ILevelUpdateView updateView, LevelBuilder typeBuilder) {
-		// TODO Auto-generated method stub
-		return null;
+		return typeBuilder.addDurationInSecondsListener((oldValue, newValue) -> updateView.updateTransitionTime(newValue))
+				//updateEnemy needs to take in the whole map and update everything
+				.addEnemyCountsListener((oldValue, newValue) -> updateView.updateEnemy(newValue))
+				.addRewardHealthListener((oldValue, newValue) -> updateView.updateRewardHealth(newValue))
+				.addRewardScoreListener((oldValue, newValue) -> updateView.updateRewardPoints(newValue))
+				.addRewardMoneyListener((oldValue, newValue) -> updateView.updateRewardMoney(newValue));
 	}
 
 }
