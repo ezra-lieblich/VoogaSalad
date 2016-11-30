284c94aaa37c2251426299275c49aaadac316898
Sean Hudson <srh50@duke.edu>
2016-11-29 23:13:07 -0500

    Worked on backend container

--- src/engine/GameData.java
+++ src/engine/GameData.java
@@ -1,5 +1,6 @@
 package engine;
 
+import java.util.Arrays;
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 import engine.enemy.EnemyManager;
@@ -10,23 +11,18 @@ import engine.weapon.WeaponTypeManager;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
 
 
-
 public class GameData {
-	private GameSettings settings;
-	private EnemyManager enemies;
-	private WeaponTypeManager weapons;
-	private TowerManager towers;
-	private PathManager path;
-	private LevelTypeManager levels;
-
+    private GameSettings settings;
+    private ManagerMediator typeManagerMediator;
     @XStreamOmitField
     private XStream Serializer = new XStream(new DomDriver());
 
-
-
-
-    public String SaveData() {
+    GameData(GameSettings settings, Manager<? extends Type>... managers) {
+        Arrays.asList(managers).forEach(a -> typeManagerMediator.addManager(a));
+        this.settings = settings;
+    }
+    
+    public String SaveData () {
         return Serializer.toXML(this);
     }
 }
-
