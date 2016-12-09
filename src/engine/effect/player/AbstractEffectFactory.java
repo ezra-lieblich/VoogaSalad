d047e91830c1e7ba6258b2c73c58239295b3dced
Sean Hudson <srh50@duke.edu>
2016-12-09 05:52:45 -0500

    Created Effect factory

--- src/engine/effect/player/AbstractEffectFactory.java
+++ src/engine/effect/player/AbstractEffectFactory.java
@@ -4,23 +4,42 @@ import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.function.BiConsumer;
 import engine.effect.Effect;
 
-public class EffectFactory {
-
+public abstract class AbstractEffectFactory {
+    
+    private final String TRIGGER_NAME;
+    private final String ENCOMPASSING_CLASS_NAME;
+    
     private Map<String, Object> effectAccessibleData;
     
-    EffectFactory() {
-        effectAccessibleData = new HashMap<String, Object>();
+    AbstractEffectFactory(String triggerVariableName, String encompasssingClassName) {
+        this.TRIGGER_NAME = triggerVariableName;
+        this.ENCOMPASSING_CLASS_NAME = encompasssingClassName;
+        this.effectAccessibleData = new HashMap<String, Object>();
+        loadDataIntoMap();
+
     }
     
-    public Object create(Effect effect) {
-//        Class<?> clazz = effect.getTrigger().getClass();
-//        Method method = effect.getTrigger().getParticipantMethod();
-        //method.invoke(obj, args) Accessible
+    public GameEffect create(Effect effect) {
+        GroovyExecutor groovyExecutor = new GroovyExecutor();
+        addEffectAccessibleData(groovyExecutor);
+        return new GameEffect(effect, groovyExecutor);
+    }
+    
+    private void loadDataIntoMap() {
+        effectAccessibleData.put(TRIGGER_NAME, null);
+        effectAccessibleData.put(ENCOMPASSING_CLASS_NAME, null);
+    }
+    
+    private void addEffectAccessibleData(GroovyExecutor groovyExecutor) {
+        effectAccessibleData.entrySet().forEach(a -> groovyExecutor.addVariable(a.getKey(), a.getValue()));
+    }
+    
+    protected void loadSpecificDataIntoMap(String key, Object value) {
+        effectAccessibleData.put(key, value);
+    }
         
-        return null;
-    }
-    
-    
 }
