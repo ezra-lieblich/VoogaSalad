2d1b86f2c023d3c5191d89d67f898ead417903f7
Sean Hudson <srh50@duke.edu>
2016-12-09 00:42:24 -0500

    Made a Groovy Executer

--- src/engine/effect/player/GroovyExecuter.java
+++ src/engine/effect/player/GroovyExecuter.java
@@ -1,5 +1,41 @@
 package engine.effect.player;
 
-public class GroovyExecuter {
+import java.util.Map;
+import java.util.Map.Entry;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
+public class GroovyExecuter {
+    private static final String INCORRECT_RETURN_TYPE = "Groovy command did not return an instance of %s";
+    private static final String INVALID_COMMAND = "Invalid Groovy command %s";
+
+    private ScriptEngine engine = new ScriptEngineManager().getEngineByName("groovy");
+        
+    public Object execute(String groovyCommand) throws GroovyException {
+        try {
+            return engine.eval(groovyCommand);
+            }
+
+        catch (ScriptException e) {
+            throw new GroovyException(e, INVALID_COMMAND, groovyCommand);
+        }
+    }
+    
+    public <E> E execute(String groovyCommand, Class<E> returnType) throws GroovyException {
+        try {
+            return returnType.cast(execute(groovyCommand));
+        }
+        catch (ClassCastException e) {
+            throw new GroovyException(e, INCORRECT_RETURN_TYPE, returnType);
+        }
+    }
+    
+    public void addVariables(Map<String, Object> variableMap) {
+        variableMap.entrySet().forEach(a -> addVariable(a.getKey(), a.getValue()));
+    }
+    
+    public void addVariable(String key, Object value) {
+        engine.put(key, value);
+    }
 }
