6e4b1f47d39a5ca711d6251512b7c7f07f3efaaa
Diane Hadley <dfh12@duke.edu>
2016-12-09 14:11:00 -0500

    Change order of editor toolbar and have game set up open first

--- src/authoring/view/AuthoringViewController.java
+++ src/authoring/view/AuthoringViewController.java
@@ -2,9 +2,13 @@ package authoring.view;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.TreeMap;
+
 import authoring.editortabpane.EditorTabPaneDelegate;
 import authoring.editorview.EditorViewController;
 import authoring.editorview.enemy.EnemyEditorViewController;
@@ -15,17 +19,20 @@ import authoring.editorview.tower.TowerEditorViewController;
 import authoring.editorview.weapon.WeaponEditorViewController;
 
 
+//TODO: Specific implementation of map
+
 public class AuthoringViewController implements EditorTabPaneDelegate {
     private IAuthoringView view;
     private HashMap<String, EditorViewController> editors;
     private final String SETTINGS_RESOURCE_PATH = "resources/ViewSettings";
     private final String TABS_RESOURCE_PATH = "resources/GameAuthoringEditorToolbar";
-    private ArrayList<String> tabNames;
+    private final String DEFAULT_TAB = "setup";
+    private List<String> tabNames;
 
     public AuthoringViewController (int width, int height) throws IOException {
         createEditors();
         createScene(width, height);
-        view.setEditorView(editors.values().iterator().next().getView());
+        view.setEditorView(editors.get(DEFAULT_TAB).getView());
     }
 
     public HashMap<String, EditorViewController> getControllers(){
@@ -45,12 +52,13 @@ public class AuthoringViewController implements EditorTabPaneDelegate {
     	editors = new HashMap<String, EditorViewController>();
         int width = Integer.parseInt(settingsResource.getString("editorPrefWidth"));
         int height = Integer.parseInt(settingsResource.getString("editorPrefHeight"));
+        editors.put("setup", new GameSettingsEditorViewController(width, height));
+        editors.put("level", new LevelEditorViewController(width, height));
         editors.put("path", new PathEditorViewController(width, height));
         editors.put("weapon", new WeaponEditorViewController(width, height));
         editors.put("enemy", new EnemyEditorViewController(width, height));
-        editors.put("tower", new TowerEditorViewController(width, height));
-        editors.put("settings", new GameSettingsEditorViewController(width, height));
-        editors.put("level", new LevelEditorViewController(width, height));
+        editors.put("tower", new TowerEditorViewController(width, height));        
+        
     }
 
     private void checkNullTabs () {
@@ -66,10 +74,22 @@ public class AuthoringViewController implements EditorTabPaneDelegate {
     private void loadInTabNames(){
         ResourceBundle tabsResource = ResourceBundle.getBundle(TABS_RESOURCE_PATH);
         Enumeration<String> nextVal = tabsResource.getKeys();
-        tabNames = new ArrayList<String>();
+        HashMap<String, String> tabMap = new HashMap<String, String>();
         while (nextVal.hasMoreElements()) {
-            tabNames.add(nextVal.nextElement());
+        	String next = nextVal.nextElement();
+        	tabMap.put(next, tabsResource.getString(next));
         }
+        
+        TreeMap<String, String> sortedTabMap = new TreeMap<String, String>(new Comparator<String>(){
+			@Override
+			public int compare(String o1, String o2) {				
+				return (tabMap.get(o1).compareTo(tabMap.get(o2)));
+			}
+        	
+        });
+        
+        sortedTabMap.putAll(tabMap);       
+        tabNames = new ArrayList<String>(sortedTabMap.keySet());
     }
     
     @Override
