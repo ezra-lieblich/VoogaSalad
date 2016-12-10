77d8b34c0afdeefb4a35c3b0272ffc3a14ecbc9c
Sean Hudson <srh50@duke.edu>
2016-12-10 05:43:08 -0500

    Added annotated data factory

--- src/engine/effect/AnnotatedMethodsFactory.java
+++ src/engine/effect/AnnotatedMethodsFactory.java
@@ -1,5 +1,28 @@
 package engine.effect;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.reflections.Reflections;
+import org.reflections.scanners.MethodAnnotationsScanner;
+
 public class AnnotatedMethodsFactory {
 
+    
+    
+    public Map<Class<?>, List<Field>> create(Class<? extends Annotation> annotation, String packageName) {
+       Reflections reflections = new Reflections(packageName, new MethodAnnotationsScanner());
+       return reflections.getFieldsAnnotatedWith(annotation).stream().collect(Collectors.groupingBy(a -> a.getDeclaringClass()));
+
+    }
+    
+    
+    
+
+    
 }
