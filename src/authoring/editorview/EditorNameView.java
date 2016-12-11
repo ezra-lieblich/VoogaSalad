68ab2081848b4ef4f24bf97ef25013c67e5fca37
andrewbihl <andrewbihlva@gmail.com>
2016-12-10 21:03:52 -0500

    Change name of IEditorView to INodeView

--- src/authoring/editorview/EditorNameView.java
+++ src/authoring/editorview/EditorNameView.java
@@ -6,7 +6,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 
 
-public abstract class EditorNameView implements IEditorView {
+public abstract class EditorNameView implements INodeView {
 
     protected HBox root;
     protected TextField nameTextField;
