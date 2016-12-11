d62d48fdc90762cd4a30576d85f07357d5cf371b
Benjamin Linde <bl144@duke.edu>
2016-12-11 18:23:36 -0500

    Fixed animation lagging with multiple towers.

--- src/gameplayer/model/weapon/Weapon.java
+++ src/gameplayer/model/weapon/Weapon.java
@@ -19,9 +19,10 @@ public class Weapon extends Observable implements IDrawable{
 	private double range;
 	private int targetEnemyID;
 	private double speedMag;
+	private int weaponTypeID;
 	
 	
-	public Weapon(String name,  double demage, double targetX, double targetY, String image,  double range, int targetID, double x, double y, double speedMag) {
+	public Weapon(String name,  double demage, double targetX, double targetY, String image,  double range, int targetID, double x, double y, double speedMag, int weaponTypeID) {
 		this.name = name;
 		this.damage = demage;
 		this.damage = 50;
@@ -34,11 +35,15 @@ public class Weapon extends Observable implements IDrawable{
 		this.yCoordinate = y;
 		this.targetEnemyID = targetID;
 		this.speedMag = speedMag;
+		this.weaponTypeID = weaponTypeID;
 	}
 	
 	
 	
 	// add boolean method out of range
+	public int getWeaponTypeID(){
+		return this.weaponTypeID;
+	}
 	
 	public int getTargetEnemyID() {
 		return targetEnemyID;
