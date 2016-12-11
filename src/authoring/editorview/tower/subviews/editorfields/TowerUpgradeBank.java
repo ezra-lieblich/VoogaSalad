00fd2cb65b8e67ac5060de5dc9f87ead4845030d
Kayla Schulz <kms114@duke.edu>
2016-12-10 21:43:44 -0500

    tower upgrade

--- src/authoring/editorview/tower/subviews/editorfields/TowerUpgradeBank.java
+++ src/authoring/editorview/tower/subviews/editorfields/TowerUpgradeBank.java
@@ -3,7 +3,9 @@ package authoring.editorview.tower.subviews.editorfields;
 import java.util.ResourceBundle;
 import authoring.editorview.tower.ITowerSetView;
 import authoring.editorview.tower.TowerAuthoringViewDelegate;
+import authoring.utilityfactories.ButtonFactory;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 
 
@@ -17,10 +19,12 @@ public class TowerUpgradeBank implements ITowerSetView {
     private HBox towerUpgradeBox;
     private TowerAuthoringViewDelegate delegate;
     private ResourceBundle labelsResource;
+    private Button addTowerUpgrade;
 
     public TowerUpgradeBank (ResourceBundle labelsResource) {
         this.labelsResource = labelsResource;
         towerUpgradeBox = new HBox(5);
+        setHBox();
     }
 
     @Override
@@ -39,6 +43,21 @@ public class TowerUpgradeBank implements ITowerSetView {
 
     public void deleteTowerUpgrade (String towerUpgrade) {
 
+    }
+
+    private void setHBox () {
+        addTowerUpgrade = ButtonFactory.makeButton("Add Upgrade", e -> dummyMethod());
+        towerUpgradeBox.getChildren().add(addTowerUpgrade);
+        towerUpgradeBox.setStyle("-fx-padding: 5;" +
+                                 "-fx-border-style: solid inside;" +
+                                 "-fx-border-width: 1;" +
+                                 "-fx-border-insets: 3;" +
+                                 "-fx-border-radius: 2;" +
+                                 "-fx-border-color: black;");
+    }
+
+    private void dummyMethod () {
+        System.out.println("Help");
     }
 
 }
