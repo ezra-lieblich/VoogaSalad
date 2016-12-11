b5af048668a1af142d1076dadb93e7460aa60197
andrewbihl <andrewbihlva@gmail.com>
2016-12-11 01:31:38 -0500

    Add method to ImageBank getIndexForItemWithIndex

--- src/authoring/editorview/ImageBank.java
+++ src/authoring/editorview/ImageBank.java
@@ -87,13 +87,22 @@ public abstract class ImageBank implements ChangeListener<Number> {
         }
     }
     
+    public Integer getIndexForItemWithID(int id){
+    	for (int i = 0; i <this.itemIDs.size(); i++){
+    		if (this.itemIDs.get(i)==id) {
+    			return i+CONTENT_OFFSET;
+    		}
+    	}
+    	return null;
+    }
+    
     /**
      * 
      * @param index of item
      * @return ID of the data object represented at that index. Will be null if there is no item there.
      */
     public Integer getIDForItemAtIndex(int index){
-    	Integer result =  this.itemIDs.get(index);
+    	Integer result =  this.itemIDs.get(index + CONTENT_OFFSET);
     	if (result<0){
     		return null;
     	}
