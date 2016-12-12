d9e08b23e1c1345ad132b4525553bf8128e6ad8b
Kayla Schulz <kms114@duke.edu>
2016-12-12 04:30:11 -0500

    added two fields to effect

--- src/authoring/editorview/collisioneffects/subviews/EffectDataView.java
+++ src/authoring/editorview/collisioneffects/subviews/EffectDataView.java
@@ -1,12 +1,14 @@
 package authoring.editorview.collisioneffects.subviews;
 
 import java.util.ResourceBundle;
-import authoring.editorview.ImageBank;
 import authoring.editorview.collisioneffects.EffectAuthoringViewDelegate;
 import authoring.editorview.collisioneffects.EffectSetView;
+import authoring.editorview.collisioneffects.subviews.editorfields.EffectConditionField;
+import authoring.editorview.collisioneffects.subviews.editorfields.EffectField;
 import authoring.editorview.collisioneffects.subviews.editorfields.EffectNameField;
 import javafx.scene.Node;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 
 /**
@@ -19,12 +21,16 @@ public class EffectDataView implements EffectSetView {
     private EffectAuthoringViewDelegate delegate;
     private BorderPane effectDataPane;
     private EffectNameField nameField;
+    private EffectConditionField conditionField;
+    private EffectField effectField;
     private ResourceBundle labelsResource;
 
     public EffectDataView () {
         labelsResource = ResourceBundle.getBundle("resources/GameAuthoringEffect");
         effectDataPane = new BorderPane();
         nameField = new EffectNameField(labelsResource);
+        conditionField = new EffectConditionField(labelsResource);
+        effectField = new EffectField(labelsResource);
         setPane();
     }
 
@@ -37,10 +43,16 @@ public class EffectDataView implements EffectSetView {
     public void setDelegate (EffectAuthoringViewDelegate delegate) {
         this.delegate = delegate;
         nameField.setDelegate(delegate);
+        conditionField.setDelegate(delegate);
+        effectField.setDelegate(delegate);
     }
 
     private void setPane () {
-        effectDataPane.setTop(nameField.getInstanceAsNode());
+        VBox vbox = new VBox(5);
+        vbox.getChildren().addAll(nameField.getInstanceAsNode(),
+                                  conditionField.getInstanceAsNode(),
+                                  effectField.getInstanceAsNode());
+        effectDataPane.setCenter(vbox);
     }
 
 }
